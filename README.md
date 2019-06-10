b62e0e3ab29a135ea05955a484e9945306050f11
sunao <sunao_0626@hotmail.com>
2019-06-11 01:53:21 +0800

    init

---
+++ README.md
@@ -0,0 +1,4 @@
+lchm
+==========
+
+[![Build Status](https://travis-ci.com/eggachecat/lchm.svg?branch=master)](https://travis-ci.com/eggachecat/lchm)
---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+const path = require('path');
+module.exports = {
+    entry: './src/index.ts',
+    mode: 'production',
+    // devtool: 'inline-source-map',
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js']
+    },
+    output: {
+        filename: '[name].[chunkhash:8].js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    optimization: {
+        runtimeChunk: true,
+        splitChunks: {
+            chunks: 'all',
+            maxInitialRequests: Infinity,
+            minSize: 0,
+            cacheGroups: {
+                vendor: {
+                    test: /[\\/]node_modules[\\/]/,
+                    name(module) {
+                        const packageName = module.context.match(/[\\/]node_modules[\\/](.*?)([\\/]|$)/)[1];
+                        return `npm.${packageName.replace('@', '')}`;
+                    },
+                }        
+            },
+        }
+    },
+};
